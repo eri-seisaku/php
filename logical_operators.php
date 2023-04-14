1aa88fb4e83146b35db3738829d32201b28b9992
eri-seisaku <eri-seisaku@gmail.com>
2023-04-14 11:22:53 +0000

    3回目

---
+++ logical_operators.php
@@ -0,0 +1,24 @@
+<?php
+
+//
+echo "論理演算子の確認\n";
+var_dump( true && false );
+var_dump( true || false );
+
+// ユーザの情報
+$age = 20;
+$point = 499;
+
+// 「かつ」の場合
+echo "\n";
+echo "「かつ」の場合\n";
+var_dump( ($age >= 20)&&($point >= 500) );
+
+// 「または」の場合
+echo "\n";
+echo "「または」の場合\n";
+var_dump( ($age >= 20)||($point >= 500) );
+
+
+
+?>
