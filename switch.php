67c24ae18450f66d1953ce9dfe42360ed216c2fd
eri-seisaku <eri-seisaku@gmail.com>
2023-04-15 14:38:06 +0000

    演習問題

--- switch.php
+++ switch.php
@@ -6,11 +6,11 @@ switch($i) {
     case 0:
         echo "i は 0 です \n";
         break;
-        
+
     case 1:
         echo "i は 1 です \n";
         break;
-        
+
     default:
         echo "i は 0と1 以外です \n";
         break;
@@ -22,14 +22,34 @@ switch($i) {
     case '2a':
         echo "i は '2a'です \n";
         break;
-        
+
     case '2':
         echo "i は '2'(string)です \n";
         break;
-        
+
     case 2:
         echo "i は 2(int)です \n";
         break;
+}
+
+// 演習問題
+
+$i = 123;
+
+if($i <= 100) {
+    echo "100以下 \n";
+} else {
+    echo "100を超える \n";
+}
+
+if($i < 50) {
+    echo "50未満 \n";
+} else if($i < 100) {
+    echo "100未満 \n";
+} else if($i < 150) {
+    echo "150未満 \n";
+} else {
+    echo "150以上 \n";
 }
 
 ?>
