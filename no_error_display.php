72de68cde5f613b9193c9684e4a0219395eff62f
eri-seisaku <eri-seisaku@gmail.com>
2023-05-13 15:01:32 +0000

    本番環境と開発環境のエラーの出力

---
+++ no_error_display.php
@@ -0,0 +1,16 @@
+<?php
+// エラーを非表示にする
+ini_set('display_errors', '0');
+error_reporting(0);
+
+// エラーを表示する
+// ini_set('display_errors', '1');
+// error_reporting(-1);
+
+
+//
+var_dump($dummy);
+//
+$awk = [];
+echo "{$awk[0]} \n";
+?>
